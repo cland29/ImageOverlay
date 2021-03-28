315e4fc24eb36ed91e0999b4bf74a57caa524f84
cland29 <carl.lee.landskron@gmail.com>
2021-03-28 15:43:50 -0400

    Add glm includes

--- Image.cpp
+++ Image.cpp
@@ -3,6 +3,12 @@
 #include "lodepng.h"
 #include <iostream>
 
+//glm
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/half_float.hpp>
+
 using namespace std;
 
 
