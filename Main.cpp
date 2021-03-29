4f4be3c1a9b8388e5df070c45a53f58bee3954d8
cland29 <carl.lee.landskron@gmail.com>
2021-03-28 21:26:25 -0400

    Update Main Function

---
+++ Main.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string>
+#include "lodepng.h"
+#include <iostream>
+
+//glm
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/half_float.hpp>
+
+
+
+int main(int argc, char **argv)
+{ 
+
+
+
+}
