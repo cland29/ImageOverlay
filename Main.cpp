a382b0b8614a5c3c107400886dd7a5e0d219c060
cland29 <carl.lee.landskron@gmail.com>
2021-03-29 18:53:38 -0400

    Add Main functions to test

--- Main.cpp
+++ Main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include "lodepng.h"
 #include <iostream>
+#include "Image.h"
 
 //glm
 #include <glm/glm.hpp>
@@ -9,10 +10,20 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/half_float.hpp>
 
-
+using namespace std;
 
 int main(int argc, char **argv)
 { 
+    string fileName;
+    if (argc > 1){
+        string arg1(argv[0]);  
+        fileName = arg1; 
+    }else{
+        fileName = "testInput.png";
+    }
+    Image test = Image(fileName);
+
+
     printf("Hello world!");
 
 
