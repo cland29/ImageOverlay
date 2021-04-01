ca35020b3ecef5029c1660da0b783585d37e0bc5
cland29 <carl.lee.landskron@gmail.com>
2021-04-01 06:22:21 -0400

    Add image object methods

---
+++ Processing/ImageOverlay/Image.py
@@ -0,0 +1,20 @@
+class myImage(object):
+    def __init__(self, filename):
+        self.img = loadImage(filename)
+        self.width = self.img.width
+        self.height = self.img.height
+    
+    def getPixel(self, x, y):
+        return self.img.pixels[y * self.width + x]
+    
+    def getPixelCol(self, x, y):
+        red = red(self.img.pixels[y * self.width + x])
+        green = green(self.img.pixels[y * self.width + x])
+        blue = blue(self.img.pixels[y * self.width + x])
+        return (red, green, blue)
+    
+    def setPixel(self, x, y, col):
+        self.img.pixels[y * self.width + x] = col
+    
+    def saveImg(self, filename):
+        self.img.save(filename)
--- Processing/ImageOverlay/ImageOverlay.pyde
+++ Processing/ImageOverlay/ImageOverlay.pyde
@@ -1,3 +1,17 @@
 #This file currently serves as a place holder so naming of the Sketch Folder was Image Overlay.
 
 #Hi future Carl! 4/1/2021
+
+import Image
+
+def setup():
+    
+    size(480, 120)
+    test = Image.myImage("test.PNG")
+    test.saveImg("test1.PNG")
+    
+    print("done")
+
+def draw():
+    
+  updatePixels()
